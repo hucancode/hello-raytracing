27229eb6eef348d497a14498e1c9fc4bf5618c39
hucancode <hucancode@gmail.com>
2024-02-27 00:16:29 +0900

    adjust color

--- src/scene/mod.rs
+++ src/scene/mod.rs
@@ -14,6 +14,7 @@ pub struct Scene {
 impl Scene {
     pub fn new() -> Self {
         let yellow = Vec3::new(0.98, 0.89, 0.69);
+        let red = Vec3::new(0.953, 0.545, 0.659);
         let base = Vec3::new(0.12, 0.12, 0.18);
         let blue = Vec3::new(0.54, 0.7, 0.98);
         let black = Vec3::new(0.06, 0.06, 0.1);
@@ -24,8 +25,8 @@ impl Scene {
                 Sphere::new_dielectric(Vec3::new(-1.0, 0.0, -1.0), 0.5, 1.5),
                 Sphere::new_lambertian(Vec3::new(0.0, 0.0, -1.0), 0.5, black),
                 Sphere::new_metal(Vec3::new(1.0, 0., -1.0), 0.5, yellow, 0.1),
-                Sphere::new_lambertian(Vec3::new(-0.7, -0.3, -0.2), 0.2, yellow),
-                Sphere::new_metal(Vec3::new(-0.3, -0.4, -0.4), 0.1, yellow, 0.9),
+                Sphere::new_lambertian(Vec3::new(-0.7, -0.3, -0.2), 0.2, red),
+                Sphere::new_metal(Vec3::new(-0.3, -0.4, -0.4), 0.1, blue, 0.9),
                 Sphere::new_dielectric(Vec3::new(0.2, -0.38, -0.16), 0.12, 0.1),
             ],
         }
