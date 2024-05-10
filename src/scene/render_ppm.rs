8b97f11f2625d112b4b1a41b0cba34f5523d96ed
Bang Nguyen Huu <hucancode@gmail.com>
2024-05-10 11:11:46 +0900

    code readability

--- src/scene/render_ppm.rs
+++ src/scene/render_ppm.rs
@@ -4,11 +4,10 @@ use std::mem::size_of;
 use std::sync::mpsc::channel;
 use wgpu::{BufferDescriptor, BufferUsages};
 
-pub fn render_ppm(renderer: &mut Renderer) -> String {
+fn copy_image_buffer(renderer: &mut Renderer) -> Vec<u8> {
     let width = renderer.config.width;
     let height = renderer.config.height;
     let size = (width * height * 3 * size_of::<f32>() as u32) as u64;
-    renderer.draw();
     let device = &renderer.device;
     let output_buffer = device.create_buffer(&BufferDescriptor {
         usage: BufferUsages::MAP_READ | BufferUsages::COPY_DST,
@@ -28,21 +27,31 @@ pub fn render_ppm(renderer: &mut Renderer) -> String {
         tx.send(result.is_ok()).unwrap()
     });
     device.poll(wgpu::Maintain::Wait);
-    let mut ret = String::new();
-    if rx.recv().is_ok_and(|success| success) {
-        let data = buffer_slice.get_mapped_range();
-        let data = bytemuck::cast_slice::<u8, f32>(data.as_ref());
-        let data = data
-            .chunks_exact(3)
-            .map(|a| [a[0], a[1], a[2]])
-            .map(|[r, g, b]| [(r * 255.0) as u8, (g * 255.0) as u8, (b * 255.0) as u8])
-            .collect::<Vec<[u8; 3]>>();
-        writeln!(ret, "P3").unwrap();
-        writeln!(ret, "{width} {height} 255").unwrap();
-        for [r, g, b] in data.iter() {
-            write!(ret, "{r} {g} {b} ").unwrap();
-        }
+    let ret = if rx.recv().unwrap_or(false) {
+        buffer_slice.get_mapped_range().to_vec()
+    } else {
+        Vec::new()
     };
     output_buffer.unmap();
+    ret
+}
+pub fn render_ppm(renderer: &mut Renderer) -> String {
+    let width = renderer.config.width;
+    let height = renderer.config.height;
+    renderer.draw();
+    let data = copy_image_buffer(renderer);
+    let mut ret = String::new();
+    let data = bytemuck::cast_slice::<u8, f32>(data.as_ref());
+    let data = data
+        .chunks_exact(3)
+        .map(|a| [a[0], a[1], a[2]])
+        .map(|[r, g, b]| [r * 255.0, g * 255.0, b * 255.0])
+        .map(|[r, g, b]| [r as u8, g as u8, b as u8])
+        .collect::<Vec<[u8; 3]>>();
+    writeln!(ret, "P3").unwrap();
+    writeln!(ret, "{width} {height} 255").unwrap();
+    for [r, g, b] in data.iter() {
+        write!(ret, "{r} {g} {b} ").unwrap();
+    }
     ret
 }
