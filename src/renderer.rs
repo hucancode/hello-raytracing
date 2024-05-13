60e180bc38eb415543bb68ed492a34f543faf163
Bang Nguyen Huu <hucancode@gmail.com>
2024-05-13 13:46:26 +0900

    upgrade wgpu 0.20

--- src/renderer.rs
+++ src/renderer.rs
@@ -1,15 +1,7 @@
 use bytemuck::{Pod, Zeroable};
 use std::{borrow::Cow, cmp::max, mem::size_of, sync::Arc};
 use wgpu::{
-    util::{BufferInitDescriptor, DeviceExt},
-    vertex_attr_array, BindGroupDescriptor, BindGroupEntry, BindGroupLayoutDescriptor,
-    BindGroupLayoutEntry, BindingType, BufferAddress, BufferBindingType, BufferDescriptor,
-    BufferUsages, Color, CommandEncoderDescriptor, Device, DeviceDescriptor, FragmentState,
-    IndexFormat, Instance, Limits, LoadOp, MultisampleState, Operations, PipelineLayoutDescriptor,
-    PrimitiveState, Queue, RenderPassColorAttachment, RenderPassDescriptor, RenderPipeline,
-    RenderPipelineDescriptor, RequestAdapterOptions, ShaderModuleDescriptor, ShaderSource,
-    ShaderStages, StoreOp, Surface, SurfaceConfiguration, Texture, TextureDescriptor,
-    TextureFormat, TextureViewDescriptor, VertexBufferLayout, VertexState, VertexStepMode,
+    util::{BufferInitDescriptor, DeviceExt}, vertex_attr_array, BindGroupDescriptor, BindGroupEntry, BindGroupLayoutDescriptor, BindGroupLayoutEntry, BindingType, BufferAddress, BufferBindingType, BufferDescriptor, BufferUsages, Color, CommandEncoderDescriptor, Device, DeviceDescriptor, FragmentState, IndexFormat, Instance, Limits, LoadOp, MultisampleState, Operations, PipelineCompilationOptions, PipelineLayoutDescriptor, PrimitiveState, Queue, RenderPassColorAttachment, RenderPassDescriptor, RenderPipeline, RenderPipelineDescriptor, RequestAdapterOptions, ShaderModuleDescriptor, ShaderSource, ShaderStages, StoreOp, Surface, SurfaceConfiguration, Texture, TextureDescriptor, TextureFormat, TextureViewDescriptor, VertexBufferLayout, VertexState, VertexStepMode
 };
 use winit::window::Window;
 
@@ -225,11 +217,13 @@ impl Renderer {
                 module: &shader,
                 entry_point: "vs_main",
                 buffers: &[Vertex::desc()],
+                compilation_options: PipelineCompilationOptions::default(),
             },
             fragment: Some(FragmentState {
                 module: &shader,
                 entry_point: "fs_main",
                 targets: &[Some(config.format.into())],
+                compilation_options: PipelineCompilationOptions::default(),
             }),
             primitive: PrimitiveState::default(),
             depth_stencil: None,
